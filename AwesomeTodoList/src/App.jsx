f2dd49d19cc489202760db5569360f61e3cd30ae
pltduchoang <plt.duchoang@gmail.com>
2023-11-02 13:12:48 -0600

    lab2

---
+++ AwesomeTodoList/src/App.jsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import ToDoForm from './ToDoForm';
+import ToDoList from './ToDoList';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ToDoList />
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+
+export default App;
